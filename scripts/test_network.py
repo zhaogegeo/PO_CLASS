dda2a1900e047f6dd2f3302c963b3409980ea722
zhaogegeo <47704682+zhaogegeo@users.noreply.github.com>
2019-02-19 18:00:04 +0800

    Update test_network.py

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -15,10 +15,10 @@ class TestNetwork:
         self.page.network.click_mobile_network()
         self.page.network.click_first_network()
         self.page.network.click_2g_network()
-        assert 1
+      
     def test_network_3g(self):
         self.page.network.click_more()
         self.page.network.click_mobile_network()
         self.page.network.click_first_network()
         self.page.network.click_3g_network()
-        assert 1
+     
